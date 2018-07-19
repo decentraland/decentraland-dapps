bcf88ff674f8f8c81e211465fe26286e645f3bec
Juan Cazala <juan@decentraland.org>
2018-07-19 19:13:59 -0300

    fix: explicit import

--- src/modules/loading/reducer.ts
+++ src/modules/loading/reducer.ts
@@ -1,9 +1,13 @@
-import { LoadingState, LoadingActions } from './types'
+import { LoadingActions } from './types'
 import { removeLast, getType, getStatus } from './utils'
+import { AnyAction } from 'redux'
 
-export const INITIAL_STATE: LoadingState = []
+export const INITIAL_STATE: AnyAction[] = []
 
-export function loadingReducer(state = INITIAL_STATE, action: LoadingActions) {
+export function loadingReducer(
+  state: AnyAction[] = INITIAL_STATE,
+  action: LoadingActions
+): AnyAction[] {
   const type = getType(action) // ie. "Fetch Address Parcels"
   const status = getStatus(action) // REQUEST, SUCCESS, FAILURE
 
