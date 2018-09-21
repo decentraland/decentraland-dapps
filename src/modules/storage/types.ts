80de37c3cd38526e5c03c6e777d0a46dd5640e3f
Juan Cazala <juan@decentraland.org>
2018-09-21 15:56:54 -0300

    fix(Storage): make some properties optional in the storage middleware

--- src/modules/storage/types.ts
+++ src/modules/storage/types.ts
@@ -2,7 +2,7 @@ import { Migrations } from '../../lib/types'
 
 export interface StorageMiddleware<T> {
   storageKey: string
-  paths: string[] | string[][]
-  actions: string[]
-  migrations: Migrations<T>
+  paths?: string[] | string[][]
+  actions?: string[]
+  migrations?: Migrations<T>
 }
