872fd1a9bed6c151b72a3cae011cceebdca8b567
Juan Cazala <juan@decentraland.org>
2018-12-14 14:02:30 -0300

    feat: added App container

---
+++ src/containers/App/App.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react'
+
+import { Page, NavbarProps, PageProps, FooterProps } from 'decentraland-ui'
+
+import Navbar from '../Navbar'
+import Footer from '../Footer'
+import { AppProps } from './App.types'
+
+export default class App extends React.PureComponent<
+  AppProps & NavbarProps & PageProps & FooterProps
+> {
+  render() {
+    const { hero, isHomePage, children, ...rest } = this.props
+    const hasHero = hero !== null && isHomePage
+    return (
+      <>
+        <Navbar {...rest as NavbarProps}>{hasHero ? hero : null}</Navbar>
+        <Page hasHero={hasHero} {...rest as PageProps}>
+          {children}
+        </Page>
+        <Footer {...rest as FooterProps} />
+      </>
+    )
+  }
+}
--- src/containers/index.ts
+++ src/containers/index.ts
@@ -1,3 +1,4 @@
+export { default as App } from './App'
 export { default as EtherscanLink } from './EtherscanLink'
 export { default as Footer } from './Footer'
 export { default as Navbar } from './Navbar'
