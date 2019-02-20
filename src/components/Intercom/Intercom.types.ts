7a04f8361a49245d6dd835ad130922f69f001176
Nicolás Santángelo <santangelonicolas@gmail.com>
2019-02-20 12:21:30 +0100

    fix: allow for partial intercom settings

--- src/components/Intercom/Intercom.types.ts
+++ src/components/Intercom/Intercom.types.ts
@@ -1,17 +1,13 @@
 export type IntercomWindow = Window & {
   Intercom?: Function
-  intercomSettings: {
-    alignment: string
-    horizontal_padding: number
-    vertical_padding: number
-  }
+  intercomSettings: IntercomSettings
 }
 
-export type IntercomSettings = {
+export type IntercomSettings = Partial<{
   alignment: 'left' | 'right'
   horizontal_padding: number
   vertical_padding: number
-}
+}>
 
 export type DefaultProps = {
   data: Record<string, any>
