8ceb6e82df83d637054d28d9c232ae1575c47446
Nicolás Santángelo <santangelonicolas@gmail.com>
2019-02-26 17:38:48 +0100

    fix: action wording

--- src/modules/modal/actions.ts
+++ src/modules/modal/actions.ts
@@ -4,14 +4,11 @@ export let modals: { [key: string]: any } = {}
 
 // Open Modal
 
-export const OPEN_MODAL = 'Open Modal'
+export const OPEN_MODAL = 'Open modal'
 
 export const getModalActions = <T>() => ({
   openModal: function(name: T, metadata: any = null) {
-    return action(OPEN_MODAL, {
-      name,
-      metadata
-    })
+    return action(OPEN_MODAL, { name, metadata })
   },
   closeModal: function(name: T) {
     return action(CLOSE_MODAL, { name })
@@ -29,7 +26,7 @@ export type OpenModalAction = ReturnType<typeof openModal>
 
 // Close Modal
 
-export const CLOSE_MODAL = 'Close Modal'
+export const CLOSE_MODAL = 'Close modal'
 
 export { closeModal }
 
@@ -37,7 +34,7 @@ export type CloseModalAction = ReturnType<typeof closeModal>
 
 // Toggle Modal
 
-export const TOGGLE_MODAL = 'Toggle Modal'
+export const TOGGLE_MODAL = 'Toggle modal'
 
 export { toggleModal }
 
@@ -45,7 +42,7 @@ export type ToggleModalAction = ReturnType<typeof toggleModal>
 
 // Close All Modals
 
-export const CLOSE_ALL_MODALS = 'Close All Modal'
+export const CLOSE_ALL_MODALS = 'Close all modals'
 
 export const closeAllModals = () => action(CLOSE_ALL_MODALS, {})
 
