bada02216d085090fc766065cb5cd037d77d7f5e
Juan Cazala <juan@decentraland.org>
2020-02-03 17:53:13 -0300

    feat: split enable and connect
    BREAKING CHANGE: if not using the WalletProvider and SignInPage components, you may need to change your connectWalletRequest to a enableWalletRequest

--- src/modules/wallet/actions.ts
+++ src/modules/wallet/actions.ts
@@ -5,7 +5,7 @@ export const CONNECT_WALLET_REQUEST = '[Request] Connect Wallet'
 export const CONNECT_WALLET_SUCCESS = '[Success] Connect Wallet'
 export const CONNECT_WALLET_FAILURE = '[Failure] Connect Wallet'
 
-export const connectWalletRequest = () => action(CONNECT_WALLET_REQUEST, {})
+export const connectWalletRequest = () => action(CONNECT_WALLET_REQUEST)
 export const connectWalletSuccess = (wallet: Wallet) =>
   action(CONNECT_WALLET_SUCCESS, { wallet })
 export const connectWalletFailure = (error: string) =>
@@ -14,3 +14,16 @@ export const connectWalletFailure = (error: string) =>
 export type ConnectWalletRequestAction = ReturnType<typeof connectWalletRequest>
 export type ConnectWalletSuccessAction = ReturnType<typeof connectWalletSuccess>
 export type ConnectWalletFailureAction = ReturnType<typeof connectWalletFailure>
+
+export const ENABLE_WALLET_REQUEST = '[Request] Enable Wallet'
+export const ENABLE_WALLET_SUCCESS = '[Success] Enable Wallet'
+export const ENABLE_WALLET_FAILURE = '[Failure] Enable Wallet'
+
+export const enableWalletRequest = () => action(ENABLE_WALLET_REQUEST)
+export const enableWalletSuccess = () => action(ENABLE_WALLET_SUCCESS)
+export const enableWalletFailure = (error: string) =>
+  action(ENABLE_WALLET_FAILURE, { error })
+
+export type EnableWalletRequestAction = ReturnType<typeof enableWalletRequest>
+export type EnableWalletSuccessAction = ReturnType<typeof enableWalletSuccess>
+export type EnableWalletFailureAction = ReturnType<typeof enableWalletFailure>
