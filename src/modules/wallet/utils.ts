f8225d8a99701a342b772e3325d6bc2525cfb6d5
Juan Cazala <juan@decentraland.org>
2020-03-25 18:59:44 -0300

    chore: readme and variable

--- src/modules/wallet/utils.ts
+++ src/modules/wallet/utils.ts
@@ -4,11 +4,15 @@ import { fromWei } from 'web3x-es/utils'
 import { MANA } from '../../contracts/MANA'
 import { Wallet } from './types'
 
-const MANA_ADDRESS = {
-  '1': '0x0f5d2fb29fb7d3cfee444a200298f468908cc942',
-  '3': '0x2a8fd99c19271f4f04b1b7b9c4f7cf264b626edb',
-  '4': '0x28bce5263f5d7f4eb7e8c6d5d78275ca455bac63',
-  '42': '0x230fc362413d9e862326c2c7084610a5a2fdf78a'
+const MANA_ADDRESS_BY_NETWORK = {
+  // Mainnet
+  1: '0x0f5d2fb29fb7d3cfee444a200298f468908cc942',
+  // Ropsten
+  3: '0x2a8fd99c19271f4f04b1b7b9c4f7cf264b626edb',
+  // Rinkeby
+  4: '0x28bce5263f5d7f4eb7e8c6d5d78275ca455bac63',
+  // Kovan
+  42: '0x230fc362413d9e862326c2c7084610a5a2fdf78a'
 }
 
 export async function getWallet() {
@@ -27,7 +31,10 @@ export async function getWallet() {
   const ethBalance = await eth.getBalance(address)
   let manaBalance = '0'
   try {
-    const mana = new MANA(eth, Address.fromString(MANA_ADDRESS[network]))
+    const mana = new MANA(
+      eth,
+      Address.fromString(MANA_ADDRESS_BY_NETWORK[network])
+    )
     manaBalance = await mana.methods.balanceOf(address).call()
   } catch (e) {
     // Temporary fix. We should detect that the user should change the network
