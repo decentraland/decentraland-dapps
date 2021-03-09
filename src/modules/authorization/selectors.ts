7733ffd5050445cef818d15550973d79c956fe91
Nicolás Santángelo <santangelonicolas@gmail.com>
2021-03-09 00:36:42 -0300

    fix: typings

--- src/modules/authorization/selectors.ts
+++ src/modules/authorization/selectors.ts
@@ -1,9 +1,11 @@
 import { getTransactionsByType } from '../transaction/selectors'
 import { getAddress } from '../wallet/selectors'
 import { GRANT_TOKEN_SUCCESS } from './actions'
+import { AuthorizationState } from './reducer'
 import { AuthorizationType } from './types'
 
-export const getState = (state: any) => state.authorization
+export const getState: (state: any) => AuthorizationState = state =>
+  state.authorization
 export const getData = (state: any) => getState(state).data
 export const getLoading = (state: any) => getState(state).loading
 export const isLoading = (state: any) => getLoading(state).length > 0
