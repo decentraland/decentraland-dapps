e05129a5709c0255f2695c5df2592535b51df8cb
Juan Cazala <juancazala@gmail.com>
2021-05-31 15:47:49 -0300

    fix: allow overriding chainId via prop

--- src/containers/TransactionLink/TransactionLink.container.ts
+++ src/containers/TransactionLink/TransactionLink.container.ts
@@ -1,15 +1,16 @@
 import { connect } from 'react-redux'
 import { getChainId } from '../../modules/wallet/selectors'
-import { MapStateProps, MapDispatchProps } from './TransactionLink.types'
+import {
+  MapStateProps,
+  MapDispatchProps,
+  OwnProps
+} from './TransactionLink.types'
 import TransactionLink from './TransactionLink'
 
-const mapState = (state: any): MapStateProps => ({
-  chainId: getChainId(state)
+const mapState = (state: any, ownProps: OwnProps): MapStateProps => ({
+  chainId: ownProps.chainId || getChainId(state)
 })
 
 const mapDispatch = (_: any): MapDispatchProps => ({})
 
-export default connect(
-  mapState,
-  mapDispatch
-)(TransactionLink)
+export default connect(mapState, mapDispatch)(TransactionLink)
