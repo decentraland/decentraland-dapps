b6eddbaac6303ee20a534e82ee71581be83e2dfa
Lautaro Petaccio <lausuper@gmail.com>
2021-07-27 17:48:28 -0300

    fix: Fix the fetchProfile method

--- src/lib/peer.ts
+++ src/lib/peer.ts
@@ -4,7 +4,7 @@ import { BaseAPI } from './api'
 import { ProfileEntity } from './types'
 
 export class PeerAPI extends BaseAPI {
-  cache: Record<string, Profile> = {}
+  cache: Record<string, Promise<Profile>> = {}
   lambdasClient: LambdasClient
 
   constructor(url: string) {
@@ -12,16 +12,22 @@ export class PeerAPI extends BaseAPI {
     this.lambdasClient = new LambdasClient(`${url}/lambdas`)
   }
 
-  // TODO, how many ways of retrieving a profile are out there?
+  /**
+   * Fetches a profile only once by caching the promise
+   * to prevent concurrent flying requests
+   *
+   * @param address - The address of the profile to retrieve.
+   */
   public async fetchProfile(address: string): Promise<Profile> {
     if (address in this.cache) {
       return this.cache[address]
     }
-    const profiles = await this.lambdasClient.fetchProfiles([
-      address.toLowerCase()
-    ])
-    this.cache[address] = profiles[0]
-    return profiles[0]
+
+    this.cache[address] = this.lambdasClient
+      .fetchProfiles([address.toLowerCase()])
+      .then()
+
+    return this.cache[address]
   }
 
   /**
