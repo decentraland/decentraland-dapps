01d2f0a37a019d634b8b1a3f2731d2331678f2d3
Juan Cazala <juancazala@gmail.com>
2021-12-23 23:53:13 -0300

    fix(hasAuthorizations): lowercase contract addresses

--- src/modules/authorization/utils.ts
+++ src/modules/authorization/utils.ts
@@ -19,10 +19,12 @@ export function hasAuthorization(
 export function areEqual(left: Authorization, right: Authorization) {
   return (
     left.type === right.type &&
-    left.authorizedAddress === right.authorizedAddress &&
-    left.contractAddress === right.contractAddress &&
+    left.authorizedAddress.toLowerCase() ===
+      right.authorizedAddress.toLowerCase() &&
+    left.contractAddress.toLowerCase() ===
+      right.contractAddress.toLowerCase() &&
     left.chainId === right.chainId &&
-    left.address === right.address
+    left.address.toLowerCase() === right.address.toLowerCase()
   )
 }
 
