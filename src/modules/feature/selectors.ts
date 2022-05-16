1f21d3cc0acb0a20b77cffdb094236531f42f530
Nando <24811313+fzavalia@users.noreply.github.com>
2022-05-16 16:03:22 -0300

    feat: Add fallback to determine the returned result instead of throwing

--- src/modules/feature/selectors.ts
+++ src/modules/feature/selectors.ts
@@ -8,23 +8,41 @@ export const getData = (state: any): Record<Applications, Feature> =>
 export const getLoading = (state: any): LoadingState => getState(state).loading
 export const getError = (state: any): string | null => getState(state).error
 
+/**
+ * Helper to get if a feature flag is whether enabled or disabled.
+ * @param state App store state.
+ * @param application Application containing the flag.
+ * @param flag Feature flag identifies.
+ * @param fallback In case the flag does not exist in the state, this will be
+ * the value returned instead of throwing an error.
+ * @returns Boolean saying if the flag is enabled or disabled.
+ */
 export const getIsFeatureEnabled = (
   state: any,
   application: Applications,
-  flag: string
+  flag: string,
+  fallback?: boolean
 ): boolean => {
-  const features = getData(state)
-  const feature: Feature | undefined = features[application]
+  try {
+    const features = getData(state)
+    const feature: Feature | undefined = features[application]
 
-  if (!feature) {
-    throw new Error(`Feature not found for application ${application}`)
+    if (!feature) {
+      throw new Error(`Feature not found for application ${application}`)
+    }
+
+    const ff: boolean | undefined = feature.flags[`${application}-${flag}`]
+
+    if (ff === undefined) {
+      throw new Error(`Flag ${flag} not found for application ${application}`)
+    }
+
+    return ff
+  } catch (e) {
+    if (fallback !== undefined) {
+      return fallback
+    }
+
+    throw e
   }
-
-  const storeFlag: boolean | undefined = feature.flags[`${application}-${flag}`]
-
-  if (storeFlag === undefined) {
-    throw new Error(`Flag ${flag} not found for application ${application}`)
-  }
-
-  return storeFlag
 }
