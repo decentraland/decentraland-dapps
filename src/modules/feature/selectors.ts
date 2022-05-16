5f9c76ead8915efa1bb368a257e59ccfb1f2a2c2
Nando <24811313+fzavalia@users.noreply.github.com>
2022-05-16 16:27:24 -0300

    feat: Get from env before getting from fetched and stored data

--- src/modules/feature/selectors.ts
+++ src/modules/feature/selectors.ts
@@ -10,11 +10,18 @@ export const getError = (state: any): string | null => getState(state).error
 
 /**
  * Helper to get if a feature flag is whether enabled or disabled.
+ * It will first look into your env file for the feature flag, if it is not defined there,
+ * it will look it in the requested and stored features data.
+ * The env key will be determined from the application and the flag. For example, if the
+ * application is "explorer" and the flag is "some-crazy-feature", it will look
+ * for it as REACT_APP_EXPLORER_SOME_CRAZY_FEATURE.
+ *
  * @param state App store state.
  * @param application Application containing the flag.
  * @param flag Feature flag identifies.
  * @param fallback In case the flag does not exist in the state, this will be
  * the value returned instead of throwing an error.
+ *
  * @returns Boolean saying if the flag is enabled or disabled.
  */
 export const getIsFeatureEnabled = (
@@ -23,6 +30,12 @@ export const getIsFeatureEnabled = (
   flag: string,
   fallback?: boolean
 ): boolean => {
+  const envValue = getFromEnv(application, flag)
+
+  if (envValue !== null) {
+    return envValue
+  }
+
   try {
     const features = getData(state)
     const feature: Feature | undefined = features[application]
@@ -45,4 +58,14 @@ export const getIsFeatureEnabled = (
 
     throw e
   }
+}
+
+const getFromEnv = (
+  application: Applications,
+  flag: string
+): boolean | null => {
+  const envify = (word: string) => word.toUpperCase().replace('-', '_')
+  const key = `REACT_APP_${envify(application)}_${envify(flag)}`
+  const value = process.env[key]
+  return !value || value === '' ? null : value === '1' ? true : false
 }
