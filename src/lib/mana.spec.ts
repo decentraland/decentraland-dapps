ff9bb019131804196d425724e78a5e7d123ae4dd
Juan Cazala <juancazala@gmail.com>
2022-05-26 09:30:42 -0300

    fix: format mana again (#253)

--- src/lib/mana.spec.ts
+++ src/lib/mana.spec.ts
@@ -1,4 +1,4 @@
-import { toFixedMANAValue } from './mana'
+import { getTrailingZeros, toFixedMANAValue } from './mana'
 
 describe('when formatting the price', () => {
   describe('when formatting alphanumeric text', () => {
@@ -10,6 +10,12 @@ describe('when formatting the price', () => {
   describe('when formatting an invalid number', () => {
     it('should return the supplied value', () => {
       expect(toFixedMANAValue('1.bc')).toBe('1.bc')
+    })
+  })
+
+  describe('when formatting an incomplete number', () => {
+    it('should return the supplied value', () => {
+      expect(toFixedMANAValue('1.')).toBe('1.')
     })
   })
 
@@ -33,13 +39,83 @@ describe('when formatting the price', () => {
     })
 
     describe('when the number has decimal zeros to the right', () => {
-      it('should remove them', () => {
-        expect(toFixedMANAValue('9.5000000')).toBe('9.5')
+      it('should remove them up to the maximum amount allowed', () => {
+        expect(toFixedMANAValue('9.5000000')).toBe('9.50')
       })
     })
+
     describe('when all the decimals are zeros', () => {
-      it('return an integer', () => {
-        expect(toFixedMANAValue('9.000')).toBe('9')
+      it('should remove them up to the maximum amount allowed', () => {
+        expect(toFixedMANAValue('9.000')).toBe('9.00')
+      })
+    })
+
+    describe('when it has the same amount of decimals as the maximum amount allowed', () => {
+      it('return the supplied value', () => {
+        expect(toFixedMANAValue('9.00')).toBe('9.00')
+      })
+    })
+
+    describe('when it has trailing zeros but they are less than the maximum amount of decimals allowed', () => {
+      it('return the supplied value', () => {
+        expect(toFixedMANAValue('9.0')).toBe('9.0')
+      })
+    })
+  })
+})
+
+describe('when getting trailing zeros', () => {
+  describe('and the input is incomplete', () => {
+    it('should return 0', () => {
+      expect(getTrailingZeros('1.')).toBe(0)
+    })
+  })
+  describe('and the input is invalid', () => {
+    it('should return 0', () => {
+      expect(getTrailingZeros('1.abc')).toBe(0)
+    })
+  })
+  describe('and the input is an integer', () => {
+    describe('and the input has no decimals', () => {
+      it('should return 0', () => {
+        expect(getTrailingZeros('1')).toBe(0)
+      })
+    })
+    describe('and the input has one decimal', () => {
+      it('should return 1', () => {
+        expect(getTrailingZeros('1.0')).toBe(1)
+      })
+    })
+    describe('and the input has two decimal', () => {
+      it('should return 1', () => {
+        expect(getTrailingZeros('1.00')).toBe(2)
+      })
+    })
+    describe('and the input has three decimal', () => {
+      it('should return 1', () => {
+        expect(getTrailingZeros('1.000')).toBe(3)
+      })
+    })
+  })
+  describe('and the input is a float', () => {
+    describe('and has no trailing zeros', () => {
+      it('should return 0', () => {
+        expect(getTrailingZeros('1.5')).toBe(0)
+      })
+    })
+    describe('and has one trailing zero', () => {
+      it('should return 1', () => {
+        expect(getTrailingZeros('1.50')).toBe(1)
+      })
+    })
+    describe('and has two trailing zeros', () => {
+      it('should return 2', () => {
+        expect(getTrailingZeros('1.500')).toBe(2)
+      })
+    })
+    describe('and has three trailing zeros', () => {
+      it('should return 3', () => {
+        expect(getTrailingZeros('1.5000')).toBe(3)
       })
     })
   })
