704fa3ed570c7b7fe21f634f41150a17220c1017
Kevin Szuchet <kevin.szuchet@decentraland.org>
2022-12-09 14:14:20 -0300

    fix: Add open mana fiat gatway missing reducers

--- src/modules/manaFiatGateway/reducer.ts
+++ src/modules/manaFiatGateway/reducer.ts
@@ -1,7 +1,13 @@
 import { loadingReducer, LoadingState } from '../loading/reducer'
 import {
   ManaFiatGatewayPurchaseCompletedFailureAction,
-  MANA_FIAT_GATEWAY_PURCHASE_COMPLETED_FAILURE
+  MANA_FIAT_GATEWAY_PURCHASE_COMPLETED_FAILURE,
+  OpenManaFiatGatewayFailureAction,
+  OpenManaFiatGatewayRequestAction,
+  OpenManaFiatGatewaySuccessAction,
+  OPEN_MANA_FIAT_GATEWAY_FAILURE,
+  OPEN_MANA_FIAT_GATEWAY_REQUEST,
+  OPEN_MANA_FIAT_GATEWAY_SUCCESS
 } from './actions'
 
 export type ManaFiatGatewayState = {
@@ -16,13 +22,38 @@ export const INITIAL_STATE: ManaFiatGatewayState = {
   error: null
 }
 
-export type ManaFiatGatewayReducerAction = ManaFiatGatewayPurchaseCompletedFailureAction
+export type ManaFiatGatewayReducerAction =
+  | OpenManaFiatGatewayRequestAction
+  | OpenManaFiatGatewaySuccessAction
+  | OpenManaFiatGatewayFailureAction
+  | ManaFiatGatewayPurchaseCompletedFailureAction
 
 export function manaFiatGatewayReducer(
   state: ManaFiatGatewayState = INITIAL_STATE,
   action: ManaFiatGatewayReducerAction
 ): ManaFiatGatewayState {
   switch (action.type) {
+    case OPEN_MANA_FIAT_GATEWAY_REQUEST: {
+      return {
+        ...state,
+        loading: loadingReducer(state.loading, action),
+        error: null
+      }
+    }
+    case OPEN_MANA_FIAT_GATEWAY_SUCCESS: {
+      return {
+        ...state,
+        loading: loadingReducer(state.loading, action)
+      }
+    }
+    case OPEN_MANA_FIAT_GATEWAY_FAILURE: {
+      const { error } = action.payload
+      return {
+        ...state,
+        loading: loadingReducer(state.loading, action),
+        error
+      }
+    }
     case MANA_FIAT_GATEWAY_PURCHASE_COMPLETED_FAILURE: {
       return {
         ...state,
