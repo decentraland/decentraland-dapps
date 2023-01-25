8a31f6a13ee3196d137f7c9a5644fffcbd362c81
Kevin Szuchet <kevin.szuchet@decentraland.org>
2023-01-25 16:34:03 -0300

    refactor: Just check if the nft prop is in the pruchase

--- src/modules/gateway/utils.ts
+++ src/modules/gateway/utils.ts
@@ -4,5 +4,5 @@ import { ManaPurchase, Purchase } from './types'
 export const purchaseEventsChannel = channel()
 
 export function isManaPurchase(purchase: Purchase): purchase is ManaPurchase {
-  return 'amount' in purchase && !('nft' in purchase)
+  return !('nft' in purchase)
 }
