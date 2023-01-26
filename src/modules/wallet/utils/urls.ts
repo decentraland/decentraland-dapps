dd5c5def09bb414833304bcff3a25ef9a3aa722f
frami <i@2fd.me>
2023-01-26 11:54:13 -0600

    Update src/modules/wallet/utils/urls.ts
    Co-authored-by: menduz <github@menduz.com>
    Signed-off-by: frami <i@2fd.me>

--- src/modules/wallet/utils/urls.ts
+++ src/modules/wallet/utils/urls.ts
@@ -1,4 +1,4 @@
-let TRANSACTIONS_API_URL = 'https://transactions-api.decentraland.co/v1'
+let TRANSACTIONS_API_URL = 'https://transactions-api.decentraland.org/v1'
 export const getTransactionsApiUrl = () => TRANSACTIONS_API_URL
 export const setTransactionsApiUrl = (url: string) =>
   (TRANSACTIONS_API_URL = url)
