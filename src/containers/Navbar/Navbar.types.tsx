c77baff6d0abf5926eded9845d4b68e4d9c16b12
Lautaro Petaccio <1120791+LautaroPetaccio@users.noreply.github.com>
2023-03-08 09:33:28 -0300

    Revert "feat: Add network alert (#380)"
    This reverts commit 1e63e947c84faca9dc5407ab98287c33febc3b3e.

--- src/containers/Navbar/Navbar.types.tsx
+++ src/containers/Navbar/Navbar.types.tsx
@@ -2,6 +2,8 @@ import { Dispatch } from 'redux'
 import { ChainId } from '@dcl/schemas/dist/dapps/chain-id'
 import { NavbarProps as NavbarComponentProps } from 'decentraland-ui/dist/components/Navbar/Navbar'
 import {
+  acceptNetworkPartialSupport,
+  AcceptNetworkPartialSupportAction,
   disconnectWallet,
   switchNetworkRequest,
   SwitchNetworkRequestAction
@@ -11,9 +13,11 @@ export type NavbarProps = NavbarComponentProps & {
   chainId?: ChainId
   appChainId: ChainId
   hasTranslations?: boolean
-  docsUrl?: string
   onSwitchNetwork: typeof switchNetworkRequest
   onSignOut: typeof disconnectWallet
+  hasAcceptedNetworkPartialSupport: boolean
+  showPartiallySupportedModal?: boolean
+  onAcceptNetworkPartialSupport: typeof acceptNetworkPartialSupport
 }
 
 export type MapStateProps = Pick<
@@ -25,10 +29,13 @@ export type MapStateProps = Pick<
   | 'hasTranslations'
   | 'chainId'
   | 'appChainId'
+  | 'hasAcceptedNetworkPartialSupport'
 >
 
 export type MapDispatchProps = Pick<
   NavbarProps,
-  'onSwitchNetwork' | 'onSignOut'
+  'onSwitchNetwork' | 'onSignOut' | 'onAcceptNetworkPartialSupport'
 >
-export type MapDispatch = Dispatch<SwitchNetworkRequestAction>
+export type MapDispatch = Dispatch<
+  SwitchNetworkRequestAction | AcceptNetworkPartialSupportAction
+>
