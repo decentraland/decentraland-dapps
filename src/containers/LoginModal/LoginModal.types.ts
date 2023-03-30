7745e4a349bba94260dbf27d3f73b3a5f0d6f59f
Kevin Szuchet <31735779+kevinszuchet@users.noreply.github.com>
2023-03-30 14:31:12 +0200

    feat: Enable opening login modal with redux (#391)
    * feat: Enable opening login modal with redux
    
    * chore: Update lock
    
    * chore: Apply PR feedback suggestions

--- src/containers/LoginModal/LoginModal.types.ts
+++ src/containers/LoginModal/LoginModal.types.ts
@@ -2,10 +2,12 @@ import { Dispatch } from 'redux'
 import { LoginModalProps } from 'decentraland-ui/dist/components/LoginModal/LoginModal'
 import { ProviderType } from '@dcl/schemas/dist/dapps/provider-type'
 import { EnableWalletRequestAction } from '../../modules/wallet/actions'
+import { ModalProps } from '../../providers/ModalProvider/ModalProvider.types'
 
 export type DefaultProps = { isLoading: boolean }
 
 export type Props = DefaultProps &
+  ModalProps &
   LoginModalProps & {
     hasTranslations?: boolean
     onConnect: (providerType: ProviderType) => any
@@ -14,6 +16,8 @@ export type Props = DefaultProps &
 export type State = {
   hasError: boolean
 }
+
+export type OwnProps = Pick<Props, 'metadata'>
 
 export type MapStateProps = Pick<
   Props,
