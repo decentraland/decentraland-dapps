c740ca5939fb5ba16e092567259c5a6bfee7173d
Lautaro Petaccio <lausuper@gmail.com>
2023-04-10 16:28:29 -0300

    feat: Add BaseClient

---
+++ src/lib/BaseClient.ts
@@ -0,0 +1,80 @@
+import signedFetch, { type AuthIdentity } from 'decentraland-crypto-fetch'
+import { URL } from 'url'
+import { ClientError } from './ClientError'
+
+const DEFAULT_RETRIES = 3
+const DEFAULT_RETRY_DELAY = 1000
+
+export type BaseClientConfig = {
+  identity?: AuthIdentity | ((...args: unknown[]) => AuthIdentity)
+  /** The number of retries if the request fails */
+  retries?: number
+  /** The delay between retries if the request fails */
+  retryDelay?: number
+}
+
+export class BaseClient {
+  private readonly baseUrl: string
+  private readonly retries: number
+  private readonly retryDelay: number
+  private readonly identity:
+    | AuthIdentity
+    | ((...args: unknown[]) => AuthIdentity)
+    | undefined
+
+  constructor(url: string, config?: BaseClientConfig) {
+    this.baseUrl = url
+    this.identity = config?.identity
+    this.retries = config?.retries ?? DEFAULT_RETRIES
+    this.retryDelay = config?.retryDelay ?? DEFAULT_RETRY_DELAY
+  }
+
+  private sleep = (delay: number) =>
+    new Promise(resolve => {
+      setTimeout(resolve, delay)
+    })
+
+  private getIdentity = () =>
+    this.identity instanceof Function ? this.identity() : this.identity
+
+  public rawFetch = (path: string, init?: RequestInit): Promise<Response> => {
+    const fullUrl = new URL(path, this.baseUrl)
+    const identity = this.getIdentity()
+    return signedFetch(fullUrl.toString(), { ...init, identity })
+  }
+
+  public fetch = async <T>(path: string, init?: RequestInit): Promise<T> => {
+    for (let attempt = 0; attempt <= this.retries; attempt++) {
+      try {
+        let response: Response
+        let parsedResponse: any
+        try {
+          response = await this.rawFetch(path, init)
+          parsedResponse = await response.json()
+        } catch (error) {
+          throw new ClientError(error.message, undefined, null)
+        }
+
+        if (!response.ok || !parsedResponse.ok) {
+          const errorMessage: string | undefined =
+            parsedResponse.message ?? parsedResponse.error
+          throw new ClientError(
+            errorMessage ??
+              `Request failed with status code ${response.status}`,
+            response.status,
+            parsedResponse.data
+          )
+        }
+        return parsedResponse.data as T
+      } catch (error) {
+        console.error(
+          `[API] HTTP request failed: ${error.message || ''}`,
+          error
+        )
+        if (this.retries === attempt) throw error
+        await this.sleep(this.retryDelay)
+      }
+    }
+    throw new Error('Unreachable code')
+  }
+}
