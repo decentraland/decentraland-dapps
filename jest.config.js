61aa95895351ba46ad29df5d6faa977c5af97902
frami <i@2fd.me>
2023-09-14 15:13:49 -0300

    chore: remove globals

--- jest.config.js
+++ jest.config.js
@@ -2,12 +2,6 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  globals: {
-    fetch: global.fetch,
-    Response: global.Response,
-    Request: global.Request,
-    Headers: global.Headers
-  },
   moduleNameMapper: {
     '\\.(css|less|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
     '\\.(gif|ttf|eot|svg)$': '<rootDir>/__mocks__/fileMock.js'
