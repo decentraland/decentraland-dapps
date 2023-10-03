040d27ae237250cdbece1cf2a7b3077b60828fb2
Lautaro Petaccio <lausuper@gmail.com>
2023-10-03 09:33:50 -0300

    fix: Asset status filter translations

--- src/containers/AssetStatusFilter/AssetStatusFilter.tsx
+++ src/containers/AssetStatusFilter/AssetStatusFilter.tsx
@@ -9,24 +9,30 @@ import { t } from '../../modules/translation/utils'
 export const AssetStatusFilter = (props: AssetStatusFilterProps) => {
   const i18n = useMemo(
     () => ({
-      title: t('asset_status_filter.title'),
+      title: t('@dapps.asset_status_filter.title'),
       status: {
-        [AssetStatus.NOT_FOR_SALE]: t('asset_status_filter.not_for_sale'),
-        [AssetStatus.ONLY_LISTING]: t('asset_status_filter.only_listings'),
-        [AssetStatus.ONLY_MINTING]: t('asset_status_filter.only_minting'),
-        [AssetStatus.ON_SALE]: t('asset_status_filter.on_sale')
+        [AssetStatus.NOT_FOR_SALE]: t(
+          '@dapps.asset_status_filter.not_for_sale'
+        ),
+        [AssetStatus.ONLY_LISTING]: t(
+          '@dapps.asset_status_filter.only_listings'
+        ),
+        [AssetStatus.ONLY_MINTING]: t(
+          '@dapps.asset_status_filter.only_minting'
+        ),
+        [AssetStatus.ON_SALE]: t('@dapps.asset_status_filter.on_sale')
       },
       tooltips: {
         [AssetStatus.NOT_FOR_SALE]: t(
-          'asset_status_filter.not_for_sale_tooltip'
+          '@dapps.asset_status_filter.not_for_sale_tooltip'
         ),
         [AssetStatus.ONLY_LISTING]: t(
-          'asset_status_filter.only_listing_tooltip'
+          '@dapps.asset_status_filter.only_listing_tooltip'
         ),
         [AssetStatus.ONLY_MINTING]: t(
-          'asset_status_filter.only_minting_tooltip'
+          '@dapps.asset_status_filter.only_minting_tooltip'
         ),
-        [AssetStatus.ON_SALE]: t('asset_status_filter.on_sale_tooltip')
+        [AssetStatus.ON_SALE]: t('@dapps.asset_status_filter.on_sale_tooltip')
       }
     }),
     []
