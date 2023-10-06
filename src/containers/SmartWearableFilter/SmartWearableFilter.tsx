eb72d0f077e55d563f4c702f3b8286fa7902db87
Juanma Hidalgo <juanma06@gmail.com>
2023-10-06 16:31:23 +0200

    feat: Add SmartWearableFilter component with its translations (#514)
    * feat: Add SmartWearableFilter component with its translations
    
    * feat: bump dcl ui

---
+++ src/containers/SmartWearableFilter/SmartWearableFilter.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { SmartWearableFilter as SmartWearableFilterUI } from 'decentraland-ui/dist/components/SmartWearableFilter'
+import { t } from '../../modules/translation/utils'
+import { SmartWearableFilterProps } from './SmartWearableFilter.types'
+
+export const SmartWearableFilter = (props: SmartWearableFilterProps) => {
+  const i18n = {
+    title: t('@dapps.smart_wearables_filter.title'),
+    selected: t('@dapps.smart_wearables_filter.selected')
+  }
+
+  return <SmartWearableFilterUI i18n={i18n} {...props} />
+}
