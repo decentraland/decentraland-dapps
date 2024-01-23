87fdafa6276fa42793f85410d211fdfa2334b069
Melisa Anabella Rossi <melisa.rossi@decentraland.org>
2024-01-23 17:32:08 -0300

    feat: add address field (#568)

---
+++ src/components/AddressField/AddressField.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {
+  AddressFieldProps,
+  AddressField as AddressFieldUI
+} from 'decentraland-ui/dist/components/AddressField'
+import { resolveName } from './utils'
+
+type Props = Omit<AddressFieldProps, 'resolveName'>
+
+export function AddressField(props: Props) {
+  return <AddressFieldUI {...props} resolveName={resolveName} />
+}
