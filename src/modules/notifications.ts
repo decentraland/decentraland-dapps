81dd23001c301ae9a09376e848fb61e465c27f0a
Braian Mellor <braianj@gmail.com>
2024-04-30 16:24:36 -0300

    feat: notification subscriptions settings

--- src/modules/notifications.ts
+++ src/modules/notifications.ts
@@ -1,12 +1,24 @@
-import { getEnv, Env } from "@dcl/ui-env";
-import { BaseClient, BaseClientConfig } from "../lib/BaseClient";
-import { DCLNotification } from "decentraland-ui/dist/components/Notifications/types"
+import { getEnv, Env } from '@dcl/ui-env'
+import {
+  Entity,
+  Scene,
+  NotificationChannelType,
+  NotificationType,
+  Subscription,
+  SubscriptionDetails
+} from '@dcl/schemas'
+import { DCLNotification } from 'decentraland-ui/dist/components/Notifications/types'
 
-export const NOTIFICATIONS_LIMIT = 50;
+import { BaseClient, BaseClientConfig } from '../lib/BaseClient'
+
+export const NOTIFICATIONS_LIMIT = 50
 
 export class NotificationsAPI extends BaseClient {
   constructor(config: BaseClientConfig) {
-    const url = getEnv() === Env.DEVELOPMENT ? "https://notifications.decentraland.zone" : "https://notifications.decentraland.org"
+    const url =
+      getEnv() === Env.DEVELOPMENT
+        ? 'https://notifications.decentraland.zone'
+        : 'https://notifications.decentraland.org'
 
     super(url, config)
   }
@@ -15,54 +27,68 @@ export class NotificationsAPI extends BaseClient {
     const params = new URLSearchParams()
 
     if (limit) {
-      params.append("limit", `${limit}`)
+      params.append('limit', `${limit}`)
     }
 
     if (from) {
-      params.append("from", `${from}`)
+      params.append('from', `${from}`)
     }
 
-    const { notifications } = await this
-    .fetch<{notifications: Array<DCLNotification>}>(`/notifications${params.toString().length ? `?${params.toString()}` : ''}`)
+    const { notifications } = await this.fetch<{
+      notifications: Array<DCLNotification>
+    }>(
+      `/notifications${params.toString().length ? `?${params.toString()}` : ''}`
+    )
 
     return notifications.map(parseNotification)
   }
 
   async markNotificationsAsRead(ids: string[]) {
-    await this.fetch('/notifications/read', { 
-      method: "PUT", 
-      body: JSON.stringify({ notificationIds: ids }), 
-      metadata: { notificationIds: ids }, 
-      headers: { "Content-Type": "application/json" }  
+    await this.fetch('/notifications/read', {
+      method: 'PUT',
+      body: JSON.stringify({ notificationIds: ids }),
+      metadata: { notificationIds: ids },
+      headers: { 'Content-Type': 'application/json' }
     })
   }
-  
+
+  async getSubscriptionSettings() {
+    await this.fetch('/subscription')
+  }
+
+  async putSubscriptionSettings(subscriptionDetails: SubscriptionDetails) {
+    await this.fetch('/subscription', {
+      method: 'PUT',
+      body: JSON.stringify(subscriptionDetails),
+      headers: { 'Content-Type': 'application/json' }
+    })
+  }
 }
 
-const ONBOARDING_KEY = "dcl_notifications_onboarding"
+const ONBOARDING_KEY = 'dcl_notifications_onboarding'
 
 export const checkIsOnboarding = () => {
-  if (typeof window !== "undefined") {    
+  if (typeof window !== 'undefined') {
     const isOnboarding = localStorage.getItem(ONBOARDING_KEY)
     if (isOnboarding) {
       const value = JSON.parse(isOnboarding)
       return value
     } else {
-      localStorage.setItem(ONBOARDING_KEY, "true")
+      localStorage.setItem(ONBOARDING_KEY, 'true')
       return true
     }
   }
 }
 
-export const setOnboardingDone  = () => {
-  if (typeof window !== "undefined") {
-    localStorage.setItem(ONBOARDING_KEY, "false")
+export const setOnboardingDone = () => {
+  if (typeof window !== 'undefined') {
+    localStorage.setItem(ONBOARDING_KEY, 'false')
   }
 }
 
 const parseNotification = (notification: DCLNotification): DCLNotification => {
-  return ({
+  return {
     ...notification,
     timestamp: Number(notification.timestamp)
-  })
+  }
 }
