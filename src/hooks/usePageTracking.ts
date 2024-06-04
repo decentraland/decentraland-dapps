ba8693460ffba2f2f88bdd9e75e047b56c5d5156
Melisa Anabella Rossi <melisa.rossi@decentraland.org>
2024-06-04 12:24:21 -0300

    feat: add new page tracking hook (#625)
    BREAKING CHANGE : Page tracking has been moved away from sagas. To continue with the tracking behavior, dapps should implement the hook usePageTracking

---
+++ src/hooks/usePageTracking.ts
@@ -0,0 +1,14 @@
+import { useEffect } from 'react'
+import { useLocation } from 'react-router'
+import { getAnalytics } from '../modules/analytics'
+
+export function usePageTracking() {
+  const location = useLocation()
+
+  useEffect(() => {
+    const analytics = getAnalytics()
+    if (analytics) {
+      analytics.page()
+    }
+  }, [location])
+}
