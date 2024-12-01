c8b4cd506be30caf6d5a5da5f8f6d116e6f7f985
LautaroPetaccio <lausuper@gmail.com>
2024-12-01 20:11:33 -0300

    feat: Add the Enhanced Intercom component

---
+++ src/containers/EnhancedIntercom/EnhancedIntercom.container.ts
@@ -0,0 +1,26 @@
+import { connect } from 'react-redux'
+import { getData as getWallet } from '../../modules/wallet/selectors'
+import Intercom from '../../components/Intercom/Intercom'
+import { MapStateProps, Props } from './EnhancedIntercom.types'
+
+const mapState = (state: any, ownProps: Props): MapStateProps => {
+  const wallet = getWallet(state)
+  const enhancedData: { Wallet?: string; 'Wallet type'?: string } = {}
+
+  if (wallet?.address) {
+    enhancedData['Wallet'] = wallet?.address.toLowerCase() ?? null
+  }
+
+  if (wallet?.providerType) {
+    enhancedData['Wallet type'] = wallet?.providerType ?? null
+  }
+
+  return {
+    data: {
+      ...enhancedData,
+      ...ownProps.data
+    }
+  }
+}
+
+export default connect(mapState)(Intercom)
