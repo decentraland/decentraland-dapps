c3d7aedac5cdfe552b84b0cbdc5b2a377b140ad2
Braian Mellor <braianj@gmail.com>
2025-07-24 18:23:19 -0300

    feat: add wallet into tracking context (#721)

--- src/modules/analytics/types.ts
+++ src/modules/analytics/types.ts
@@ -1,8 +1,29 @@
 import { AnyAction } from 'redux'
 
-export interface AnalyticsWindow extends Window {
-  analytics: SegmentAnalytics.AnalyticsJS
+export type AnalyticsWithMiddleware = SegmentAnalytics.AnalyticsJS & {
+  addSourceMiddleware?: (middleware: MiddlewareFunction) => void
+  _walletMiddlewareRegistered?: boolean
 }
+export interface AnalyticsWindow extends Window {
+  analytics: AnalyticsWithMiddleware
+}
+
+type SegmentPayload = {
+  type: 'track' | 'identify' | 'page'
+  obj: {
+    properties?: Record<string, unknown>
+    traits?: Record<string, unknown>
+    context?: Record<string, unknown>
+  }
+}
+
+export type MiddlewareFunction = ({
+  payload,
+  next
+}: {
+  payload: SegmentPayload
+  next: (payload: SegmentPayload) => void
+}) => void
 
 export type ActionType = AnyAction['type']
 export type EventName = string | ((action: AnyAction) => string)
@@ -14,4 +35,38 @@ export interface AnalyticsAction {
   actionType: ActionType
   eventName?: EventName
   getPayload?: GetPayload
+}
+
+export interface WindowWithWallets extends Window {
+  ethereum?: Ethereum & {
+    // rabby injected provider: https://rabby.io/docs/integrating-rabby-wallet/
+    isRabby?: boolean
+    // metamask injected provider: https://docs.metamask.io/wallet/reference/provider-api/#ismetamask
+    isMetaMask?: boolean
+    // coinbase injected provider: https://docs.cdp.coinbase.com/coinbase-wallet/introduction/coinbase-wallet-injected-ethereum-provider
+    isCoinbaseWallet?: boolean
+  }
+  solana?: {
+    isPhantom?: boolean
+    isMathWallet?: boolean
+  }
+  sollet?: any
+  braveSolana?: {
+    isBraveWallet?: boolean
+  }
+  solong?: any
+  exodus?: {
+    solana?: any
+  }
+  glowSolana?: any
+  coin98?: any
+  coinbaseSolana?: any
+  clover_solana?: any
+  Slope?: any
+  huobiWallet?: {
+    isHuobiWallet?: boolean
+  }
+  nightly?: {
+    solana?: any
+  }
 }
