a398cb2b25634c4ee3209b33685858bfd00ba48e
Lautaro Petaccio <1120791+LautaroPetaccio@users.noreply.github.com>
2025-10-27 14:53:39 -0300

    fix: Get correct contract name from trade when cancelling (#735)
    * fix: Get correct contract name from trade when cancelling
    
    * fix: Remove unused import

--- src/modules/trades/TradeService.ts
+++ src/modules/trades/TradeService.ts
@@ -1,10 +1,6 @@
 import { Trade, TradeCreation } from '@dcl/schemas'
 import { AuthIdentity } from 'decentraland-crypto-fetch'
-import {
-  ContractName,
-  getContract,
-  getContractName
-} from 'decentraland-transactions'
+import { getContract, getContractName } from 'decentraland-transactions'
 import { TradesAPI } from './api'
 import { getOnChainTrade } from '../../lib/trades'
 import { sendTransaction } from '../wallet/utils'
@@ -39,7 +35,7 @@ export class TradeService {
   }
 
   async cancel(trade: Trade, sentBeneficiaryAddress: string) {
-    const contractName = trade.contract as ContractName
+    const contractName = getContractName(trade.contract)
     const contract = getContract(contractName, trade.chainId)
     const tradeToCancel = getOnChainTrade(trade, sentBeneficiaryAddress)
     return sendTransaction(contract, 'cancelSignature', [tradeToCancel])
